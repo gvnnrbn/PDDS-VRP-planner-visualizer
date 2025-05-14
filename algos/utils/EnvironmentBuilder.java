6171df79dcd0e65bad8e7ff5c6d71274b89e1869
gvnnrbn <g.urbinagarro@gmail.com>
2025-05-13 19:18:30 -0500

    fix filter state parameters into env

--- algos/utils/EnvironmentBuilder.java
+++ algos/utils/EnvironmentBuilder.java
@@ -1,6 +1,7 @@
 package utils;
 
 import domain.*;
+import scheduler.EnumVehicleState;
 import scheduler.ScheduleState;
 import scheduler.SchedulerBlockage;
 import scheduler.SchedulerOrder;
@@ -18,8 +19,8 @@ public class EnvironmentBuilder {
         Time maxDeadline = currentTime.addMinutes(scMinutes);
 
         List<Vehicle> environmentVehicles = convertToVehicles(state.vehicles);
-        List<Warehouse> environmentWarehouses = convertToWarehouses(state.warehouses);
-        List<Blockage> environmentBlockages = convertToBlockages(state.blockages);
+        List<Warehouse> environmentWarehouses = convertToWarehouses(state.warehouses, state.vehicles);
+        List<Blockage> environmentBlockages = convertToBlockages(state.blockages, state.currentTime);
         List<Order> environmentOrders = convertToOrders(state.orders);
         environmentOrders = environmentOrders.stream()
             .filter(o -> o.releaseTime().isBefore(maxDeadline) && o.deadline().isAfter(currentTime))
@@ -30,25 +31,34 @@ public class EnvironmentBuilder {
 
     private static List<Vehicle> convertToVehicles(List<SchedulerVehicle> original) {
         return original.stream()
+            .filter(v -> v.state == EnumVehicleState.IDLE || v.state == EnumVehicleState.ONTHEWAY )
             .map(v -> new Vehicle(v.id, v.weight, v.maxFuel,
             v.currentFuel, v.maxGLP, v.currentGLP, v.position))
             .collect(Collectors.toList());
     }
 
-    private static List<Warehouse> convertToWarehouses(List<SchedulerWarehouse> original) {
-        return original.stream()
+    private static List<Warehouse> convertToWarehouses(List<SchedulerWarehouse> original, List<SchedulerVehicle> stuckedVehicles) {
+        List<Warehouse> list = original.stream()
             .map(w -> new Warehouse(w.id, w.position, w.currentGLP, w.maxGLP, w.isMain, w.wasVehicle))
             .collect(Collectors.toList());
+        for(SchedulerVehicle v : stuckedVehicles){
+            if(v.state == EnumVehicleState.STUCK){
+                list.add(new Warehouse(v.id+1000, v.position, v.currentGLP, v.maxGLP, false, true));
+            }
+        }
+        return list;
     }
     
-    private static List<Blockage> convertToBlockages(List<SchedulerBlockage> original) {
+    private static List<Blockage> convertToBlockages(List<SchedulerBlockage> original, Time currentTime) {
         return original.stream()
+        .filter(b -> ! b.endTime.isBeforeOrAt(currentTime))
         .map(b -> new Blockage(b.vertices))
         .collect(Collectors.toList());
     }
     private static List<Order> convertToOrders(List<SchedulerOrder> original) {
         return original.stream()
-            .map(o -> new Order(o.id, o.amountGLP,o.position, o.deadline, o.arrivalTime))
+            .filter(o -> o.deliverTime == null)
+            .map(o -> new Order(o.id, o.amountGLP, o.position, o.deadline, o.arrivalTime))
             .collect(Collectors.toList());
     }
 }
