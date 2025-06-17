52f3baa9c82c26546a9af21f85ca26043d323eca
T3DoomV3 <165855620+T3DoomV3@users.noreply.github.com>
2025-06-17 01:40:17 -0500

    Movimiento con nuevo Json // Cambios a algunos types front

--- frontend/src/core/types/manetenimiento.ts
+++ frontend/src/core/types/manetenimiento.ts
@@ -1,12 +1,10 @@
-import type { TipoVehiculo } from "./vehiculo";
-
 export interface MantenimientoSimulado {
     idMantenimiento: number;
     vehiculo: {
         placa: string;
-        tipo: TipoVehiculo
+        tipo: string
     }
-    estado: "En Curso" | "Programado" | "Terminado";
+    estado: string;
     fechaInicio: string;
     fechaFin: string; 
 }
